d5d2bb871c47ec618bcc9a5435b0c344ce9d6e20
Denis Pilshikov <pilshikovdenis@yandex.ru>
2022-12-21 12:37:18 +0300

    Initial commit

---
+++ src/main/java/com/pilshikov/news_checker/NewsCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.pilshikov.news_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsCheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pilshikov/news_checker/NewsCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilshikov.news_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
