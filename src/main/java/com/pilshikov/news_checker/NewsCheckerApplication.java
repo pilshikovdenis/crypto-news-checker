1f2f4a86fc14ddb0fb04d5d7ab330951e240775f
Denis Pilshikov <pilshikovdenis@yandex.ru>
2022-12-21 13:39:19 +0300

    News parsing jobs was added

--- src/main/java/com/pilshikov/news_checker/NewsCheckerApplication.java
+++ src/main/java/com/pilshikov/news_checker/NewsCheckerApplication.java
@@ -2,8 +2,10 @@ package com.pilshikov.news_checker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NewsCheckerApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/pilshikov/news_checker/job/ParseLatestNewsTask.java
@@ -0,0 +1,62 @@
+package com.pilshikov.news_checker.job;
+
+
+import com.pilshikov.news_checker.model.News;
+import com.pilshikov.news_checker.service.NewsService;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class ParseLatestNewsTask {
+    private final String URL = "https://forklog.com/news";
+
+    @Autowired
+    private NewsService newsService;
+
+    @Scheduled(fixedDelay = 10000)
+    public void runParsing() {
+        try {
+            // get html page
+            Document document = Jsoup.connect(URL)
+                    .userAgent("Mozilla")
+                    .timeout(5000)
+                    .referrer("https://www.google.ru/")
+                    .get();
+
+            // get all html containers with news description
+            Elements newsBlocks = document.select(".post_item");
+
+            // parse each html block to find news data
+            for (Element elem: newsBlocks) {
+                String title = elem.select(".text_blk p").text();
+                if (newsService.isExists(title)) continue;
+
+
+                String short_text = elem.select(".text_blk .post_excerpt").text();
+
+                Element link = elem.select("a").first();
+                assert link != null;
+                String url = link.attr("href");
+
+                System.out.println("title : " + title);
+                System.out.println("short_text: " + short_text);
+                System.out.println("url: " + url);
+
+                News news = new News();
+                news.setTitle(title);
+                news.setShortText(short_text);
+                newsService.save(news);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
